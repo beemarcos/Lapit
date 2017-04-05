d0f79d9b4cddbb01c87321937288663f5d1d9b52
beemarcos <beemarcos72@gmail.com>
2017-04-05 10:53:36 -0300

    Redondinho sem Login

--- app/src/main/java/com/example/marcos/lapit/SignInActivity.java
+++ app/src/main/java/com/example/marcos/lapit/SignInActivity.java
@@ -3,8 +3,15 @@ package com.example.marcos.lapit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    private Button mLoginBtn;
 
     private Toolbar mSignInToolbar;
 
@@ -15,6 +22,17 @@ public class SignInActivity extends AppCompatActivity {
 
         mSignInToolbar = (Toolbar)findViewById(R.id.signInToolbar);
         setSupportActionBar(mSignInToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mEmailField = (EditText)findViewById(R.id.tfUsuario);
+        mPasswordField = (EditText)findViewById(R.id.tfSenha);
+
+
+
+
+
+
+
 
 
     }
