2788f3c0287267c5eaba290739753e99626af2fc
beemarcos <beemarcos72@gmail.com>
2017-04-06 11:08:45 -0300

    Comecando a gerencia de usuario

--- app/src/main/java/com/example/marcos/lapit/SignInActivity.java
+++ app/src/main/java/com/example/marcos/lapit/SignInActivity.java
@@ -1,37 +1,91 @@
 package com.example.marcos.lapit;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
 
-    private EditText mEmailField;
-    private EditText mPasswordField;
+    private EditText nEmailField;
+    private EditText nPasswordField;
 
-    private Button mLoginBtn;
+    private Button nLoginBtn;
+    private Toolbar nSignInToolbar;
 
-    private Toolbar mSignInToolbar;
+    private FirebaseAuth nAuth;
 
-    @Override
+    private FirebaseAuth.AuthStateListener nAuthListner;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        mSignInToolbar = (Toolbar)findViewById(R.id.signInToolbar);
-        setSupportActionBar(mSignInToolbar);
+        nAuth = FirebaseAuth.getInstance();
+
+        nSignInToolbar = (Toolbar)findViewById(R.id.signInToolbar);
+        setSupportActionBar(nSignInToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mEmailField = (EditText)findViewById(R.id.tfUsuario);
-        mPasswordField = (EditText)findViewById(R.id.tfSenha);
+        nEmailField = (EditText)findViewById(R.id.tfUsuario);
+        nPasswordField = (EditText)findViewById(R.id.tfSenha);
+        nLoginBtn = (Button) findViewById(R.id.buttonEntrar);
+
+        nAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()!= null){
+                    //parei aqui
+                }else{
+                    //scsc
+                }
+
+            }
+        };
+
+        nLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignIn();
+            }
+        });
 
 
 
 
 
 
+    }
+
+    private void startSignIn(){
+        String email = nEmailField.getText().toString();
+        String password = nPasswordField.getText().toString();
+
+        if(TextUtils.isEmpty(email)||TextUtils.isEmpty(password)){
+            Toast.makeText(SignInActivity.this,"Preencha todos os campos!",Toast.LENGTH_LONG).show();
+        }else{
+            nAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(SignInActivity.this,"Problema no login!",Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+            });
+        }
 
 
 
